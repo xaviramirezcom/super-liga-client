4605e8d4bd1fc5053be24b8f1014857b2694df33
Xavier Ramirez <xaviercobain88@hotmail.com>
2014-10-20 23:18:17 -0500

    signup

--- www/src/js/controllers.js
+++ www/src/js/controllers.js
@@ -579,22 +579,20 @@ angular.module('app.controllers', ['pascalprecht.translate', 'ngCookies'])
         }])
 
     // signup controller
-    .controller('SignupFormController', ['$scope', '$http', '$state', function ($scope, $http, $state) {
+    .controller('SignupFormController', ['$scope', '$http', '$state', 'restServices', function ($scope, $http, $state, restServices) {
         $scope.user = {};
+        $scope.app.customSettings.accessPages = true;
         $scope.authError = null;
-        $scope.signup = function () {
-            $scope.authError = null;
-            // Try to create
-            $http.post('api/signup', {name: $scope.user.name, email: $scope.user.email, password: $scope.user.password})
-                .then(function (response) {
-                    if (!response.data.user) {
-                        $scope.authError = response;
-                    } else {
-                        $state.go('app.dashboard-v1');
-                    }
-                }, function (x) {
-                    $scope.authError = 'Server Error';
-                });
+
+
+        $scope.signUp = function () {
+            restServices.post('user/signup',
+                {name: $scope.user.name, email: $scope.user.email, password: $scope.user.password}
+            ).then(function (response) {
+                    $state.go('app.dashboard-v1');
+                }, function () {
+                    $scope.authError = 'Sorry, something goes wrong';
+                })
         };
     }])
     // tab controller
